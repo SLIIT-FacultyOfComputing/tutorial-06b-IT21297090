9761a582199d976e981b71ff816325a31f49eb9d
Chamodi Maheesha <99491092+IT21297090@users.noreply.github.com>
2022-03-17 04:25:14 +0000

    setters and getters is completed

--- Box.h
+++ Box.h
@@ -4,8 +4,16 @@ class Box {
        int width;
        int height;
     public:
-       // write prototypes of setters for length, width and height
+
+      void setWidth(int w);
+      void setLength(int l);
+      void setHeight(int h);
+      int getWidth();
+      int getLength();
+      int getHeight();
+      int calcVolume();
+// write prototypes of setters for length, width and height
        // write prototypes of getters for length, width and height
  
-       int calcVolume();
+       
 };
