e348e91ac22fa055ef2ad413fbcf3d2cb02c6c06
Chamodi Maheesha <99491092+IT21297090@users.noreply.github.com>
2022-03-17 04:04:59 +0000

    implement method is completed

--- Student.cpp
+++ Student.cpp
@@ -4,11 +4,17 @@
 using namespace std;
 
 // Assign studentId and name
-void Student::assignDetails(int no) {
+void Student::assignDetails(int no,char name[]) {
+  studentID=no;
+  strcpy(studentName,name)
   
 }
 
 // Display StudentId and Name
-Student::display() {
+void Student::display() {
+  cout<<"Student ID"<<studentID<<endl;
+
+  cout<<"Student name :"<<
+    studentName<<endl;
   
 }
